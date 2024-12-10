9e4ca920c676b09fb57ba4d65627a60740155084
ascpial <mail@ascpial.fr>
2024-12-10 17:27:55 +0100

    Day 10 I love graphs

---
+++ 2024/day10/star1.py
@@ -0,0 +1,38 @@
+input = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+
+with open('input.txt', 'r') as file:
+    input = file.read()
+
+map = [[int(c) for c in i.strip()] for i in input.strip().split("\n")]
+
+def get(x, y):
+    if 0 <= x < len(map[0]) and 0 <= y < len(map):
+        return map[y][x]
+
+def score(x, y):
+    i = get(x, y)
+    if i == 9:
+        return {(x, y)}
+    targets = set()
+    for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        if get(x+dx, y+dy) == i+1:
+            targets.update(score(x+dx, y+dy))
+    return targets
+
+somme = 0
+
+for y in range(len(map)):
+    for x in range(len(map[0])):
+        if get(x, y) == 0:
+            test=score(x, y)
+            # print(len(test), test)
+            somme += len(test)
+
+print(somme)
