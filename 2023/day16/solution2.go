dd34f768787ca5b666a6d762a56afb54303d02e0
ascpial <mail@ascpial.fr>
2025-11-26 09:10:41 +0100

    single file for day 16

--- 2023/day16/solution2.go
+++ 2023/day16/solution2.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 )
 
-// try to push
-
 var example = `.|...\....
 |.-.\.....
 .....|-...
@@ -104,6 +102,81 @@ type Traverse struct {
 	X int
 	Y int
 	D Direction
+}
+
+func Count(entrypoint Traverse) int {
+	for _, row := range contraption {
+		for _, cell := range row {
+			cell.Energized = false
+			cell.Seen = make(map[Direction]bool, 4)
+		}
+	}
+	height := len(contraption)
+	width := len(contraption[0])
+	queue := []Traverse{entrypoint}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		for _, dir := range contraption[cur.Y][cur.X].GoThrough(cur.D) {
+			switch dir {
+			case Left:
+				if cur.X > 0 {
+					queue = append(queue, Traverse{cur.X - 1, cur.Y, dir})
+				}
+			case Right:
+				if cur.X < width-1 {
+					queue = append(queue, Traverse{cur.X + 1, cur.Y, dir})
+				}
+			case Up:
+				if cur.Y > 0 {
+					queue = append(queue, Traverse{cur.X, cur.Y - 1, dir})
+				}
+			case Down:
+				if cur.Y < height-1 {
+					queue = append(queue, Traverse{cur.X, cur.Y + 1, dir})
+				}
+			}
+		}
+	}
+	counter := 0
+	for _, row := range contraption {
+		for _, cell := range row {
+			if cell.Energized {
+				counter++
+				// 	fmt.Print("#")
+				// } else {
+				// 	fmt.Print(".")
+			}
+		}
+		// fmt.Print("\n")
+	}
+	return counter
+}
+
+func Star1() {
+	fmt.Printf("%d\n", Count(Traverse{0, 0, Right}))
+}
+
+func Star2() {
+	height := len(contraption)
+	width := len(contraption[0])
+	entrypoints := []Traverse{}
+	for x := range width {
+		entrypoints = append(entrypoints, Traverse{x, 0, Down})
+		entrypoints = append(entrypoints, Traverse{x, height - 1, Up})
+	}
+	for y := range height {
+		entrypoints = append(entrypoints, Traverse{0, y, Right})
+		entrypoints = append(entrypoints, Traverse{width - 1, y, Left})
+	}
+	maxCount := 0
+	for _, entrypoint := range entrypoints {
+		count := Count(entrypoint)
+		if count > maxCount {
+			maxCount = count
+		}
+	}
+	fmt.Printf("%d\n", maxCount)
 }
 
 func main() {
