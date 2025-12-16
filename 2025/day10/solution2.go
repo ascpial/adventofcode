32e3f428e29b6bce5c9a6f7d09aac66b4c169952
ascpial <mail@ascpial.fr>
2025-12-16 09:09:24 +0100

    this works ig

--- 2025/day10/solution2.go
+++ 2025/day10/solution2.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"container/heap"
 	_ "embed"
 	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -17,7 +19,6 @@ var example = `[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 var input string
 
 type Machine struct {
-	Target   int
 	Buttons  [][]int
 	Joltages []int
 }
@@ -51,116 +52,67 @@ func ParseMachine(rawMachine string) Machine {
 		requirements = append(requirements, requirement)
 	}
 
-	return Machine{0, buttons, requirements}
+	return Machine{buttons, requirements}
 }
 
-type Move struct {
-	Current   []int
-	Presses   int
-	Estimated int
+func InlineSum(values []int) string {
+	if len(values) == 1 {
+		return fmt.Sprintf("b%d", values[0])
+	} else {
+		return fmt.Sprintf("(+ b%d %s)", values[0], InlineSum(values[1:]))
+	}
 }
 
-// Define a custom type for the heap
-type PriorityQueue []Move
+var pattern = regexp.MustCompile(` (\d+)\)`)
 
-// Implement the heap.Interface for IntHeap
-
-// Len is the number of elements in the collection.
-func (h PriorityQueue) Len() int { return len(h) }
-
-// Less reports whether the element with index i should sort before the element with index j.
-func (h PriorityQueue) Less(i, j int) bool {
-	return h[i].Estimated < h[j].Estimated // Min-heap: smallest element should be at the top
-}
-
-// Swap swaps the elements with indexes i and j.
-func (h PriorityQueue) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-// Push adds an element to the heap.
-func (h *PriorityQueue) Push(x any) {
-	*h = append(*h, x.(Move))
-}
-
-// Pop removes and returns the smallest element from the heap.
-func (h *PriorityQueue) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-func HashStatus(status []int) string {
-	// fmt.Printf("%v\n", status)
-	// hashed := []string{}
-	// for _, a := range status {
-	// 	hashed = append(hashed, strconv.FormatInt(int64(a), 10))
-	// }
-	return fmt.Sprintf("%v", status)
-}
-
-func MinDist(current, target []int) int {
-	minDist := ^int(0)
-	for i := range len(current) {
-		if target[i]-current[i] < minDist {
-			minDist = target[i] - current[i]
+func Solve(machine Machine) int {
+	query := ""
+	for button := range len(machine.Buttons) {
+		query += fmt.Sprintf("(declare-const b%d Int)\n", button)
+	}
+	allJoltages := make([][]int, len(machine.Joltages))
+	allButtons := []int{}
+	for buttonID, button := range machine.Buttons {
+		for _, joltageID := range button {
+			allJoltages[joltageID] = append(allJoltages[joltageID], buttonID)
 		}
+		query += fmt.Sprintf("(assert (>= b%d 0))\n", buttonID)
+		allButtons = append(allButtons, buttonID)
 	}
-	return minDist
-}
-
-func Compatible(current, target []int) bool {
-	for i := range len(current) {
-		if current[i] > target[i] {
-			return false
+	for i, joltage := range machine.Joltages {
+		query += fmt.Sprintf("(assert (= %s %d))\n", InlineSum(allJoltages[i]), joltage)
+	}
+	query += fmt.Sprintf("(minimize %s)\n", InlineSum(allButtons))
+	query += "(check-sat)\n(get-model)\n"
+	// fmt.Print(query)
+	fi, err := os.Create("solution2.z3")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
 		}
+	}()
+	fi.Write([]byte(query))
+	cmd := exec.Command("z3", "solution2.z3")
+	stdout, err := cmd.Output()
+	if err != nil {
+		panic(err)
 	}
-	return true
-}
 
-func Add(current []int, things []int) []int {
-	current2 := make([]int, len(current))
-	copy(current2, current)
-	for _, thing := range things {
-		current2[thing]++
-	}
-	return current2
-}
+	allValues := pattern.FindAllStringSubmatch(string(stdout), -1)
 
-func AStar(machine Machine) int {
-	// fmt.Printf("Target: %v\nButtons: %v\n", machine.Joltages, machine.Buttons)
-	visited := map[string]struct{}{}
-
-	pq := &PriorityQueue{}
-	target := HashStatus(machine.Joltages)
-
-	heap.Init(pq)
-	start := make([]int, len(machine.Joltages))
-	heap.Push(pq, Move{start, 0, MinDist(start, machine.Joltages)})
-
-	for pq.Len() > 0 {
-		cur := heap.Pop(pq).(Move)
-		// fmt.Printf("%v\n", cur)
-
-		for _, things := range machine.Buttons {
-			next := Add(cur.Current, things)
-			hashedNext := HashStatus(next)
-			if hashedNext == target {
-				return cur.Presses + 1
-			}
-			if Compatible(next, machine.Joltages) {
-				// fmt.Printf("%s\n", hashedNext)
-				_, seen := visited[hashedNext]
-				if !seen {
-					heap.Push(pq, Move{next, cur.Presses + 1, cur.Presses + 1 + MinDist(next, machine.Joltages)})
-				}
-			}
+	total := 0
+	for _, values := range allValues {
+		value, err := strconv.Atoi(values[1])
+		if err != nil {
+			panic(err)
 		}
+		total += value
 	}
 
-	panic("not found what is going on aaaaaaaah")
+	return total
 }
 
 func main() {
@@ -171,7 +123,7 @@ func main() {
 
 	minPresses := 0
 	for _, machine := range machines {
-		minPresses += AStar(machine)
+		minPresses += Solve(machine)
 	}
 	fmt.Printf("%d\n", minPresses)
 }
